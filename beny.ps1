9bc54e42b87a75aabb7d60eba5147949544e9e70
ben <122939326+beneggen2@users.noreply.github.com>
2023-11-23 19:32:48 -0700

    Add files via upload

---
+++ beny.ps1
@@ -0,0 +1,9 @@
+importScripts { 
+
+    fetch debugger 
+    debugger to File name tp.widget.bootstrap.js
+     import File from URL -[]
+    u
+
+} to function start() 
+importScripts to https://widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.js
